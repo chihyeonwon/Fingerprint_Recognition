616742d81b9cb3136eb97767fb2adf3477b768e5
ylose <heungeob1003@gmail.com>
2024-06-30 16:25:45 +0900

    지문 인식 기능 구현

--- app/src/main/java/com/wonchihyeon/fingerprint/MainActivity.kt
+++ app/src/main/java/com/wonchihyeon/fingerprint/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.wonchihyeon.fingerprint
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import me.aflak.libraries.FingerprintCallback
+import me.aflak.libraries.FingerprintDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+
+            // FingerPrintDialog로 지문인증을 쉽게 사용 가능
+            // 사용법은 AlertDialog의 빌더 패턴과 유사
+            FingerprintDialog.initialize(this)
+                .title("지문인증")
+                .message("지문으로 인증합니다.")
+                .callback(object : FingerprintCallback {
+                        // 인증 성공인 경우의 콜백 함수
+                        override fun onAuthenticationSuccess() {
+                            Toast.makeText(applicationContext, "인증", Toast.LENGTH_SHORT).show()
+                        }
+
+                    // 인증 실패인 경우의 콜백 함수
+                    override fun onAuthenticationCancel() {
+                        Toast.makeText(applicationContext, "인증 실패", Toast.LENGTH_SHORT).show()
+                    }
+                })
+                .show()
         }
     }
 }
